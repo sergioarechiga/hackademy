b3a9bec8c93ce7847a8cd59dd7e267694462d462
Oigres <sergioarechiga@gmail.com>
2019-06-13 20:45:12 -0600

    Correccion de ejercicio 4 ya que no entrega resultado correcto

--- e4.php
+++ e4.php
@@ -4,23 +4,31 @@ $a = 999;
 $b = 999;
 $fin = 0;
 
+$s_a = 0;
+$s_b = 0;
+$solucion = 0;
+
 print "Find the largest palindrome made from the product of two 3-digit numbers.<br>";
 
-while ( !$fin ){
 
-    $derecho =$a * $b;  //multiplicacion y cadena al derecho
-    $ohcered = strrev($derecho);  //inversa
+for ($a=100;$a<1000;$a++) {
+    for ($b=100;$b<1000;$b++){
+        $derecho =$a * $b;  //multiplicacion y cadena al derecho
+        $ohcered = strrev($derecho);  //inversa
 
-    if ( $derecho == $ohcered ){
-        print "$a x $b = $derecho<br>";
-        $fin = 1;
-    }
+        if ( $derecho == $ohcered ) {
 
-    if ( $a == 100 ) {
-        $a = 999;
-        $b--;
-    } else {
-        $a--;
+            if ( $derecho > $solucion ){
+                $s_a = $a;
+                $s_b = $b;
+                $solucion = $derecho;
+            }
+
+//            print "$a x $b = $derecho<br>";
+//            $fin = 1;
+        }
+
     }
 
 }
+print "$s_a x $s_b = $solucion<br>";
